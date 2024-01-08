916b58c04c18b5056f7c5ac2e2f9d39d087eb5dc
The Waggle <artuoh76@gmail.com>
2024-01-08 17:39:55 +0100

    Добавьте файлы проекта.

---
+++ SP7_Dz_31.08/Bus.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP7_Dz_31._08
+{
+    public class Bus
+    {
+        public int occupiedSeats = 0;
+        public int totalSeats;
+
+
+
+        public void rideToTheStopAndAgain(ref int peopleAtBusStop, AutoResetEvent resetEvent, int busId)
+        {
+            Thread.Sleep(500);
+            while (true)
+            {
+                resetEvent.WaitOne();
+                Console.WriteLine();
+                Console.WriteLine($"Bus with ID {busId} arrived!");
+                Console.WriteLine($"People at buss stop {peopleAtBusStop}");
+                int pickedPassenger;
+                if (peopleAtBusStop <= (totalSeats - occupiedSeats))
+                {
+                    occupiedSeats += peopleAtBusStop;
+                    pickedPassenger = peopleAtBusStop;
+                    peopleAtBusStop = 0;
+                }
+                else
+                {
+                    pickedPassenger = totalSeats - occupiedSeats;
+                    occupiedSeats = totalSeats;
+                    peopleAtBusStop -= pickedPassenger;
+                }
+                Console.WriteLine($"Bus with ID {busId} picked up {pickedPassenger} passenger.");
+                Console.WriteLine();
+
+                resetEvent.Set();
+                Thread.Sleep(3000);
+                occupiedSeats = 0;
+            }
+        }
+
+        public Bus(int totalSeats)
+        {
+            this.totalSeats = totalSeats;
+            Console.WriteLine($"Created bus with {totalSeats} seats");
+        }
+
+
+    }
+}
---
+++ SP7_Dz_31.08/Program.cs
@@ -0,0 +1,31 @@
+﻿using SP7_Dz_31._08;
+
+List<Bus> buses = new List<Bus>(5);
+Random rand = new Random();
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Bus with ID {i}: ");
+    Bus bus = new Bus(rand.Next(10, 20));
+    buses.Add(bus);
+}
+Console.WriteLine();
+Console.WriteLine();
+
+int peopleAtTheStop = 50;
+AutoResetEvent resetEvent = new AutoResetEvent(true);
+for (int i = 0; i < buses.Count; i++)
+{
+    Thread thread = new Thread(() =>
+    {
+        buses[i].rideToTheStopAndAgain(ref peopleAtTheStop, resetEvent, i);
+    });
+    thread.Start();
+    Thread.Sleep(500);
+}
+while (true)
+{
+    int countOfNewPeople = rand.Next(20, 30);
+    peopleAtTheStop += countOfNewPeople;
+    Console.WriteLine($"New people at stop: {countOfNewPeople}. Total count of people: {peopleAtTheStop}");
+    Thread.Sleep(1000);
+}
